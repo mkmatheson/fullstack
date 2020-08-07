12d5ef1dc6fa08b506829a3277e6e335e18a2f94
McKay Matheson <mckaymatheson@gmail.com>
2020-08-07 14:07:24 -0600

    use ejs to pass templated code

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 import config from "./config";
-//fs isn't needed when I'm using .use
-// import fs from "fs";
 import apiRouter from "./api";
 
 import express from "express";
 const server = express();
 
+//express takes care of everything else if I just add this line
+server.set("view engine", "ejs");
+
 server.get("/", (req, res) => {
-  res.send("Helloooooo Express!");
+  //   res.send("Helloooooo Express!");
+  res.render("index", {
+    content: "Hello Express and EJS!",
+  });
 });
 
 server.use("./api", apiRouter);
-
-//note that about.html is moved to the public folder
 server.use(express.static("public"));
-
-// server.get("/about.html", (req, res) => {
-//   //   res.send("this is the about page!\n");
-//   fs.readFile("./about.html", (err, data) => {
-//     res.send(data.toString());
-//   });
-// });
 
 server.listen(config.port, () => {
   console.info("Express listening on port ", config.port);
